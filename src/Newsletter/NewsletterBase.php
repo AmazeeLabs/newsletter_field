5d05ed3fd562c3d945e489fdf666972f175f374f
Vasile Chindris <vasile.chindris@amazee.com>
2016-01-12 12:14:24 +0100

    NewsletterBase objects can now receive injected parameters.

--- src/Newsletter/NewsletterBase.php
+++ src/Newsletter/NewsletterBase.php
@@ -6,10 +6,18 @@
  */
 
 namespace Drupal\newsletter_field\Newsletter;
+use Drupal\Core\DependencyInjection\ContainerInjectionInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * The base class for all the newsletter plugins.
  */
-abstract class NewsletterBase implements NewsletterPluginInterface {
+abstract class NewsletterBase implements NewsletterPluginInterface, ContainerInjectionInterface {
 
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static();
+  }
 }
